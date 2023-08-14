58152ae1703d5c45a8536478f1aea2592c46891b
ToritseTuedor <torisevalerie@gmail.com>
2023-08-13 22:09:57 -0400

    asset images added, read me file updated

---
+++ firebaseConfig.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBat7ebqWQcS9Wc_FwCdws-4UxfiVLj7DA",
+
+  authDomain: "skinnedbynature-d88fe.firebaseapp.com",
+
+  projectId: "skinnedbynature-d88fe",
+
+  storageBucket: "skinnedbynature-d88fe.appspot.com",
+
+  messagingSenderId: "778030815440",
+
+  appId: "1:778030815440:web:dab6264314b03a1b55a6d5",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
--- scripts.js/script.js
+++ scripts.js/script.js
@@ -1,3 +1,19 @@
+// Connecting Firebase
+
+import app from "../firebaseConfig.js";
+import {
+  getDatabase,
+  ref,
+  get,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+
+// reference the database
+
+const database = getDatabase(app);
+
+// reference the inventory section of the database 
+
+
 // Location-Changer
 
 // When the user clicks on the flag a drop down menu appears
@@ -125,3 +141,5 @@ skincareDropDownMenu.addEventListener("mouseleave", () => {
   isSkincareMenuHovered = false;
   handleSkinCareMouseLeave();
 });
+
+// Add to Cart
